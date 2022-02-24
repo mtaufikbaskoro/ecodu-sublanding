57db5e9cbf3644583898ebe6d63efb2057f4b019
mtaufikbaskoro <mtaufikbaskoro@gmail.com>
2022-02-24 13:59:05 +0700

    changing default image

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return(
         <div className="xl:w-lesson-card md:w-[200px] mx-2 md:mx-0 flex flex-col items-center justify-start gap-6 bg-primary-color text-additional-color-200 hover:bg-additional-color-200 hover:text-primary-color border-0 hover:border-4 hover:border-additional-color-400 transition ease-in-out rounded-md">
             <div className="pt-2 px-2">
-                <Image src="/assets/download.jfif" alt="image-lesson" width={300} height={200} className="xl:w-11/12 rounded-md border-0" />
+                <Image src="/assets/download.jpg" alt="image-lesson" width={300} height={200} className="xl:w-11/12 rounded-md border-0" />
             </div>
             <div className="text-center">
                 <h3 className="text-2xl">{props.title}</h3>
